81fd9651932f9289fca1a71645475336b3bb5e2a
Heritage <ireanuoluwah@gmail.com>
2025-01-24 00:36:56 +0100

    updated database config

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$servername = "localhost"; // Hostname (usually 'localhost' for local development)
-$username = "root";        // MySQL username (default is 'root' for XAMPP)
-$password = "";            // MySQL password (default is an empty string for XAMPP)
-$dbname = "shop_db";       // Name of your database
+$servername = "dpg-cu9c20pu0jms73fdgctg-a"; // Hostname (usually 'localhost' for local development)
+$username = "shop_db_reet_user";        // MySQL username (default is 'root' for XAMPP)
+$password = "YjZKbnHi2Nn66vIWEVjl2jk4FaQYJ9C2";            // MySQL password (default is an empty string for XAMPP)
+$dbname = "shop_db_reet";       // Name of your database
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
