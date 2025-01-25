45f628b71812ce0f5faff3019e27196b467f42bf
Heritage <ireanuoluwah@gmail.com>
2025-01-25 13:20:04 +0100

    switched to pg script

--- db.php
+++ db.php
@@ -5,11 +5,11 @@ $username = "heritage";        // MySQL username (default is 'root' for XAMPP)
 $password = "lT8HwZaAotGHn8vf4Tfb3feq4mMGA56a";            // MySQL password (default is an empty string for XAMPP)
 $dbname = "shop_db_k62y";       // Name of your database
 
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Establish the connection
+$connection = pg_connect("host=$servername dbname=$dbname user=$username password=$password");
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Check if the connection was successful
+if (!$connection) {
+    die("Database connection failed: " . pg_last_error());
 }
 ?>
