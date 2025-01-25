b39d1d252026aeec742de639a703e7a43fd3d85a
Heritage <ireanuoluwah@gmail.com>
2025-01-25 13:01:37 +0100

    changed hostname

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 // Database configuration
-$servername = "dpg-cua37n9u0jms73flkl60-a.oregon-postgres.render.com"; // Hostname (usually 'localhost' for local development)
+$servername = "dpg-cua37n9u0jms73flkl60-a"; // Hostname (usually 'localhost' for local development)
 $username = "heritage";        // MySQL username (default is 'root' for XAMPP)
 $password = "lT8HwZaAotGHn8vf4Tfb3feq4mMGA56a";            // MySQL password (default is an empty string for XAMPP)
 $dbname = "shop_db_k62y";       // Name of your database
