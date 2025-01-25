51155b8c168b8beb29a21a360122d1e557efeabb
Heritage <ireanuoluwah@gmail.com>
2025-01-25 12:27:54 +0100

    i think it should work now

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$servername = "dpg-cu9c20pu0jms73fdgctg-a"; // Hostname (usually 'localhost' for local development)
-$username = "shop_db_reet_user";        // MySQL username (default is 'root' for XAMPP)
-$password = "YjZKbnHi2Nn66vIWEVjl2jk4FaQYJ9C2";            // MySQL password (default is an empty string for XAMPP)
-$dbname = "shop_db_reet";       // Name of your database
+$servername = "dpg-cua37n9u0jms73flkl60-a.oregon-postgres.render.com"; // Hostname (usually 'localhost' for local development)
+$username = "heritage";        // MySQL username (default is 'root' for XAMPP)
+$password = "lT8HwZaAotGHn8vf4Tfb3feq4mMGA56a";            // MySQL password (default is an empty string for XAMPP)
+$dbname = "shop_db_k62y";       // Name of your database
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
